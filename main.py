0ea1530b13abd7d2617702f0476d63e6e560b7e2
JunxiBao <baojunxilogin@icloud.com>
2024-11-01 21:34:37 +0800

    change size and collor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ input_string = input("请输入要生成二维码的字符串：")
 
 # 创建二维码
 qr = qrcode.QRCode(
-    version=1,  # 控制二维码的大小，范围是1到40，数字越大越复杂
+    version=40,  # 控制二维码的大小，范围是1到40，数字越大越复杂
     error_correction=qrcode.constants.ERROR_CORRECT_L,  # 控制二维码的容错率
     box_size=1,  # 控制二维码每个小格子的像素数
     border=0,  # 控制二维码边框的宽度（单位为格子数）
@@ -16,7 +16,7 @@ qr.add_data(input_string)
 qr.make(fit=True)
 
 # 生成图片
-img = qr.make_image(fill='yellow', back_color='blue')
+img = qr.make_image(fill='black', back_color='white')
 
 # 保存图片文件
 img.save("QR.png")
